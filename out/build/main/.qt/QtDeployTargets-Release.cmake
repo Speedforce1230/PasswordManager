bbc9d5b4774784ff0b9e7dff244afc5a2c341737
Vasu Dhall <gamedhall@gmail.com>
2024-12-23 20:18:40 +0530

    removed unneccessary files

--- out/build/main/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PasswordManager_FILE F:/QT_Password_Manager/out/build/main/Release/PasswordManager.exe)
-set(__QT_DEPLOY_TARGET_PasswordManager_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PasswordManager_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Core.dll)
